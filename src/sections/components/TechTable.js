e288578b3b0b0d478a344a624376385e04659e70
Sasha Czerniawski <sashacandoit@gmail.com>
2023-04-06 08:02:38 -0400

    changed TechTable to use SimpleGrid

--- src/sections/components/TechTable.js
+++ src/sections/components/TechTable.js
@@ -1,35 +1,28 @@
 import React from "react";
-import { Table, Tbody, Tr, Td  } from '@chakra-ui/react'
+import { SimpleGrid, Box, Text } from '@chakra-ui/react'
 import { about_me_tech } from "../../data/portfolio_data"
 import { ChevronRightIcon } from '@chakra-ui/icons'
 
 
 function TechTable() {
   return (
-    <Table size="sm">
-      <Tbody>
-        {about_me_tech.map((rowContent, rowID) => (
-          <TableRow rowContent={rowContent} key={rowID} />
+    <SimpleGrid marginTop={3} minChildWidth="140px" columns={5} spacingX={4} spacingY={1}>
+      {about_me_tech.map((itemContent, itemId) => (
+          <TechItem itemContent={itemContent} key={itemId} />
         ))}
-      </Tbody>
-    </Table>
+    </SimpleGrid>
   );
 }
 
-function TableRow(props) {
-  const { rowContent } = props;
+function TechItem(props) {
+  const { itemContent } = props;
 
   return (
-    <Tr>
-      {rowContent.map((val, rowID) => (
-        <Td
-          borderWidth="0"
-          paddingLeft={"0"}
-          fontSize='sm'
-          w="20%"
-          key={rowID}><ChevronRightIcon color='red.300' />{val}</Td>
+    <Box>
+      {itemContent.map((val, itemId) => (
+          <Box fontSize='sm' key={itemId} minH="2em"><ChevronRightIcon color='red.300' />{val}</Box>
       ))}
-    </Tr>
+    </Box>
   );
 }
 
