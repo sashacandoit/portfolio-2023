86c8b2180799cf0c569d32df698556585b1801a3
Sasha Czerniawski <sashacandoit@gmail.com>
2023-04-06 07:29:15 -0400

    added ProjectRow component with orientation prop from parent

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Grid, GridItem, SimpleGrid, Box } from '@chakra-ui/react'
+import { Container, Grid, GridItem, SimpleGrid, Box, Image } from '@chakra-ui/react'
 import { featured_projects } from "../data/portfolio_data"
 import SectionHeading from "./components/SectionHeading";
 
@@ -17,17 +17,49 @@ function Projects() {
         <GridItem border='1px' borderColor='pink.300' area={'title'}>
           <SectionHeading title='Projects' />
         </GridItem>
-        <GridItem paddingTop="2rem" border='1px' borderColor='orange.300' area={'projects'}>
+        <GridItem paddingTop="3rem" border='1px' borderColor='orange.300' area={'projects'}>
           
-          <SimpleGrid columns={2} spacing='5'>
-            <Box border='1px' borderColor='#1E90FF' height='200px'></Box>
-            <Box border='1px' borderColor='#1E90FF' height='200px'></Box>
-          </SimpleGrid>
+          <ProjectRow orientation="left" />
+          <ProjectRow orientation="right" />
+
         </GridItem>
       </Grid>
     </Container>
   )
   
 };
+
+function ProjectRow(props) {
+  const { orientation } = props;
+  
+  if (orientation === "right") {
+    return (
+      <SimpleGrid marginBottom="5em" columns={2} spacing='5'>
+        <Box border='1px' borderColor='#1E90FF' height='20rem'>
+          <Image
+            boxSize='100%'
+            objectFit='cover'
+            src='https://placehold.co/600x400'
+            alt='Sasha Czerniawski'
+          />
+        </Box>
+        <Box border='1px' borderColor='#1E90FF' height='20rem'></Box>
+      </SimpleGrid>
+    )
+  }
+  return (
+    <SimpleGrid marginBottom="5em" columns={2} spacing='5'>
+        <Box border='1px' borderColor='#1E90FF' height='20rem'></Box>
+        <Box border='1px' borderColor='#1E90FF' height='20rem'>
+          <Image
+            boxSize='100%'
+            objectFit='cover'
+            src='https://placehold.co/600x400'
+            alt='Sasha Czerniawski'
+          />
+        </Box>
+      </SimpleGrid>
+    )
+}
 
 export default Projects;
