f86d8fa4f4500b5f722db6807289fc4a87d8b347
Sasha Czerniawski <sashacandoit@gmail.com>
2023-04-06 08:38:52 -0400

    added technologies list to project details

--- src/sections/components/ImageRows.js
+++ src/sections/components/ImageRows.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Grid, GridItem, SimpleGrid, Box, Image, Text, Heading } from '@chakra-ui/react'
+import { Container, Grid, HStack, SimpleGrid, Box, Image, Text, Heading } from '@chakra-ui/react'
 
 function LeftImgRow(props) {
   const { rowContent } = props;
@@ -17,6 +17,11 @@ function LeftImgRow(props) {
         <Text color='red.300' fontSize="sm">Featured Project</Text>
         <Heading as='h4' size='md'>{rowContent.title}</Heading>
         <Text fontSize="sm">{rowContent.description}</Text>
+        <HStack spacing={2} marginTop={3} >
+          {rowContent.technologies.map((tech, id) => (
+            <Box key={id} fontSize="xs" >{tech}</Box>
+          ))}
+        </HStack>
       </Box>
     </SimpleGrid>
   )
@@ -30,6 +35,11 @@ function RightImgRow(props) {
         <Text color='red.300' fontSize="sm">Featured Project</Text>
         <Heading as='h4' size='md'>{rowContent.title}</Heading>
         <Text fontSize="sm">{rowContent.description}</Text>
+        <HStack spacing={2} marginTop={3}>
+          {rowContent.technologies.map((tech, id) => (
+            <Box key={id} fontSize="xs" >{tech}</Box>
+          ))}
+        </HStack>
       </Box>
       <Box border='1px' borderColor='#1E90FF' height='20rem'>
         <Image
@@ -42,5 +52,7 @@ function RightImgRow(props) {
     </SimpleGrid>
   )
 }
+
+
 
 export {LeftImgRow, RightImgRow}
