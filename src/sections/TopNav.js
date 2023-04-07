ca70c451b8ddf93e3b5e33eebbe85cb4fb12691a
Sasha Czerniawski <sashacandoit@gmail.com>
2023-04-07 15:25:39 -0400

    added mobile menu icon and MobileMenu component drawer

--- src/sections/TopNav.js
+++ src/sections/TopNav.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Flex, Spacer, Box } from '@chakra-ui/react' //layout
+import { Flex, Spacer, Box, Show, Hide, IconButton } from '@chakra-ui/react' //layout
 import { Heading } from '@chakra-ui/react' //typography
 import { ButtonGroup, Button } from '@chakra-ui/react'
+import { MenuSvg } from "../assets/icons/icons";
 
 function TopNav() {
   return (
@@ -10,13 +11,23 @@ function TopNav() {
         <Heading size='md'>Sasha Czerniawski</Heading>
       </Box>
       <Spacer />
-      <ButtonGroup gap='2'>
-        <Button colorScheme='black' variant='link'>About</Button>
-        <Button colorScheme='black' variant='link'>Experience</Button>
-        <Button colorScheme='black' variant='link'>Projects</Button>
-        <Button colorScheme='black' variant='link'>Contact</Button>
-        <Button colorScheme='red' variant='outline'>Resume</Button>
-      </ButtonGroup>
+      <Show above='sm'>
+        <ButtonGroup gap='2'>
+          <Button colorScheme='black' variant='link'>About</Button>
+          <Button colorScheme='black' variant='link'>Experience</Button>
+          <Button colorScheme='black' variant='link'>Projects</Button>
+          <Button colorScheme='black' variant='link'>Contact</Button>
+          <Button colorScheme='red' variant='outline'>Resume</Button>
+          <IconButton colorScheme='white' icon={<MenuSvg color='black' fontSize="2em" />} />
+
+          
+        </ButtonGroup>
+      </Show>
+
+      <Show below='sm'>
+
+      </Show>
+      
     </Flex>
   )
 }
---
+++ src/sections/components/MobileMenu.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Flex, Spacer, Box, Show, Hide, IconButton } from '@chakra-ui/react' //layout
+import { Heading } from '@chakra-ui/react' //typography
+import { ButtonGroup, Button } from '@chakra-ui/react'
+
+function MobileMenu() {
+
+}
+
+export default MobileMenu;
