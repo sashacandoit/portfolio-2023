09c2fdc08e1738e5f004d5f21280204bc694fd5d
Sasha Czerniawski <sashacandoit@gmail.com>
2023-05-01 07:30:31 -0400

    added wrapper container to page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Show, Grid, GridItem } from '@chakra-ui/react'
+import { Show, Grid, GridItem, Container } from '@chakra-ui/react'
 import RightNav from './sections/RightNav';
 import LeftNav from './sections/LeftNav'
 import Welcome from './sections/Welcome'
@@ -13,46 +13,48 @@ import GetInTouch from './sections/GetInTouch';
 
 function App() {
   return (
-    <Grid
-      templateAreas={
-        `"header header header" 
+    <Container maxWidth={"100vw"}>
+      <Grid
+        templateAreas={
+          `"header header header" 
       "left-nav main right-nav"
       "footer footer footer"`}
-      gridTemplateRows={'60px 1fr 50px'}
-      gridTemplateColumns={'75px 1fr 75px'}
-      gap='1'
-    >
-    
-      <GridItem p='2' area={'header'} textAlign={'center'}>
-        <TopNav />
-      </GridItem>
+        gridTemplateRows={'60px 1fr 50px'}
+        gridTemplateColumns={'75px 1fr 75px'}
+        gap='1'
+      >
 
-      <Show above='md'>
-        <GridItem area={'left-nav'}>
-          <LeftNav />
+        <GridItem p='2' area={'header'} textAlign={'center'}>
+          <TopNav />
         </GridItem>
-      </Show>
-      
-      <GridItem area={'main'} align='start'>
-        <Welcome />
-        <About />
-        <Work />
-        <Projects />
-        <OtherProjects />
-        <GetInTouch />
-      </GridItem>
 
-      <GridItem area={'footer'} textAlign={'center'}>
-        <Footer />
-      </GridItem>
+        <Show above='md'>
+          <GridItem area={'left-nav'}>
+            <LeftNav />
+          </GridItem>
+        </Show>
 
-      <Show above='md'>
-        <GridItem area={'right-nav'}>
-          <RightNav />
+        <GridItem area={'main'} align='start'>
+          <Welcome />
+          <About />
+          <Work />
+          <Projects />
+          <OtherProjects />
+          <GetInTouch />
         </GridItem>
-      </Show>
-      
-    </Grid>
+
+        <GridItem area={'footer'} textAlign={'center'}>
+          <Footer />
+        </GridItem>
+
+        <Show above='md'>
+          <GridItem area={'right-nav'}>
+            <RightNav />
+          </GridItem>
+        </Show>
+
+      </Grid>
+    </Container>
   );
 }
 
